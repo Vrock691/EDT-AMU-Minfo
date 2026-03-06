43cb577c18d5322b3f1c06863a82a0418359ab5b
Valentin Mary <valentin.mary@etu.univ-amu.fr>
2026-03-06 10:57:38 +0100

    feat: Add a custom thank you event

--- src/eventManager.go
+++ src/eventManager.go
@@ -16,6 +16,7 @@ func StartPeriodicFetching() {
 	// Fetch immediately
 	fmt.Println("Fetching calendar...")
 	cal, err = ics.ParseCalendarFromUrl(generateURL())
+	addCustomEvent()
 	if err != nil {
 		fmt.Println("Error fetching calendar:", err)
 		return
@@ -25,6 +26,7 @@ func StartPeriodicFetching() {
 	for range ticker.C {
 		fmt.Println("Fetching calendar...")
 		cal, err = ics.ParseCalendarFromUrl(generateURL())
+		addCustomEvent()
 		if err != nil {
 			fmt.Println("Error fetching calendar:", err)
 			continue
@@ -34,4 +36,18 @@ func StartPeriodicFetching() {
 
 func generateURL() string {
 	return "https://ade-web-consult.univ-amu.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?projectId=8&resources=1819&calType=ical&firstDate=2025-09-01&lastDate=2026-09-19"
+}
+
+func addCustomEvent() {
+	event := cal.AddEvent(fmt.Sprintf("thankyou-%d@vamary.fr", time.Now().Unix()))
+	event.SetCreatedTime(time.Now())
+	event.SetDtStampTime(time.Now())
+	event.SetModifiedAt(time.Now())
+
+	event.SetStartAt(time.Date(2026, 4, 10, 0, 0, 0, 0, time.UTC))
+	event.SetEndAt(time.Date(2026, 4, 10, 23, 59, 59, 59, time.UTC))
+	event.SetSummary("Merci d'avoir utilisé mon script !")
+	event.SetDescription("Merci d'avoir utilisé mon script !")
+	event.SetURL("https://vamary.fr/")
+	event.SetOrganizer("valentin.mary@proton.me", ics.WithCN("Valentin Mary"))
 }
